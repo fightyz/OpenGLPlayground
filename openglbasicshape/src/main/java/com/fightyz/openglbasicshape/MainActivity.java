986dfd50b3e4ca6e6e51d9fc90d263d7753e583d
Zoe Young <yangzhou1989@gmail.com>
2020-01-24 16:59:25 +0800

    可以画三角形

--- openglbasicshape/src/main/java/com/fightyz/openglbasicshape/MainActivity.java
+++ openglbasicshape/src/main/java/com/fightyz/openglbasicshape/MainActivity.java
@@ -29,31 +29,11 @@ public class MainActivity extends AppCompatActivity {
 
         glSurfaceView = new GLSurfaceView(this);
 
-        // Check if the system supports OpenGL ES 2.0
-        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        // Request an OpenGL ES 2.0 compatible context.
+        glSurfaceView.setEGLContextClientVersion(2);
 
-        final boolean supportsEs2 =
-                configurationInfo.reqGlEsVersion >= 0x20000
-                        || (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1
-                        && (Build.FINGERPRINT.startsWith("generic")
-                        || Build.FINGERPRINT.startsWith("unknown")
-                        || Build.MODEL.contains("google_sdk")
-                        || Build.MODEL.contains("Emulator")
-                        || Build.MODEL.contains("Android SDK built for x86")));
-
-        if (supportsEs2) {
-            // Request an OpenGL ES 2.0 compatible context.
-            glSurfaceView.setEGLContextClientVersion(2);
-
-            // Assign our renderer.
-            glSurfaceView.setRenderer(new TriangleRenderer(this));
-            rendererSet = true;
-        } else {
-            Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
-                    Toast.LENGTH_LONG).show();
-            return;
-        }
+        // Assign our renderer.
+        glSurfaceView.setRenderer(new TriangleRenderer(this));
         setContentView(glSurfaceView);
     }
 
